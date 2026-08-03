331ea9169e8948784b5a00738a21b9adbe366ae5
KennethCarter1 <kenneth.jared1004@gmail.com>
2026-08-02 20:07:30 -0500

    fix hash para github pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+function quitarHash() {
+    history.replaceState(null, '', window.location.pathname);
+}
+
 document.querySelectorAll('.nav-superior a').forEach(function(enlace) {
     enlace.addEventListener('click', function() {
-        setTimeout(function() {
-            history.replaceState(null, '', window.location.pathname);
-        }, 0);
+        setTimeout(quitarHash, 10);
     });
 });
+
+window.addEventListener('load', function() {
+    if (window.location.hash) {
+        setTimeout(quitarHash, 100);
+    }
+});
+
+window.addEventListener('hashchange', quitarHash);
